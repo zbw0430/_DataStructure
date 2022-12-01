628c276298b43d0ac7fbf12b72b68de86a8df968
zbw0430 <691189575@qq.com>
2022-12-01 10:53:55 +0800

    no message

--- OJ练习/Practice/Practice/main.c
+++ OJ练习/Practice/Practice/main.c
@@ -801,6 +801,23 @@
 //    return ret;
 //}
 
+
+//力扣 226. 翻转二叉树
+//struct TreeNode* invertTree(struct TreeNode* root)
+//{
+//    if(root == NULL)
+//        return NULL;
+//
+//    struct TreeNode* tmp = root->left;
+//    root->left = root->right;
+//    root->right = tmp;
+//
+//    invertTree(root->left);
+//    invertTree(root->right);
+//
+//    return root;
+//}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
